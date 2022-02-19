832803fb98557171c78bb830ddbf6ea0e19ce42a
Nicholas Stewart <stewartnick53@icloud.com>
2022-02-19 14:03:00 -0600

    updating http to https

--- src/API.js
+++ src/API.js
@@ -3,11 +3,11 @@ const deck_id = "23e28hmqkev7";
 export const API_CALLS = {
   drawCards: async (numOfCards) => {
     return fetch(
-      `http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${numOfCards}`
+      `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${numOfCards}`
     );
   },
 
   shuffleCards: () => {
-    fetch(`http://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
+    fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
   },
 };
